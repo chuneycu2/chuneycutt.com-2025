4f3a934958c7088f1201f7118013e0c7610366cf
Cyrus Huneycutt <chuneycu@gmail.com>
2025-12-10 12:29:22 -0500

    begin data setup for sidebar

--- src/components/1-pages/Home.jsx
+++ src/components/1-pages/Home.jsx
@@ -5,13 +5,16 @@ import Content from "../2-templates/Content.jsx";
 import DataParser from "../data/DataParser.js";
 
 export default function Home() {
+
+    // Fetch page data
     const data  = useLoaderData();
     const pageData = DataParser(data);
-    console.log(pageData);
+    const thisPage = pageData[0].acf; // get only ACF data here
+
     return (
         <div className="container-main home row m-0">
-            <Sidebar/>
-            <Content/>
+            <Sidebar title={thisPage.title} intro={thisPage.intro} footnote={thisPage.footnote} />
+            <Content content={thisPage.components_react} />
             {/* TODO: add transitions between pages with useTransition */}
         </div>
     )
--- src/components/2-templates/Titles.jsx
+++ src/components/2-templates/Titles.jsx
@@ -1,13 +1,12 @@
-export default function Titles() {
+export default function Titles(props) {
     {/* merge SectionTitles.jsx into this component */}
     return (
         <div className="title-container">
             {/* programmatically add title info based on page URL */}
-            <h1>Cyrus Huneycutt</h1>
+            <h1>{props.title}</h1>
             <h2>Front End Developer</h2>
             <p className="intro">
-                I build lean, scalable, and component-driven web applications with a bias for pixel-perfect user
-                experience.
+                {props.intro}
             </p>
         </div>
     )
