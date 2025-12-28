964bde01ec7bde234797945dc09163e63ae70bb9
Cyrus Huneycutt <chuneycu@gmail.com>
2025-12-28 08:49:03 -0500

    adds error boundary to root path for 404, etc

---
+++ src/components/ux/ErrorElement.jsx
@@ -0,0 +1,44 @@
+import { useRouteError, isRouteErrorResponse } from "react-router";
+import ProjectLinks from "../5-atoms/ProjectLinks.jsx";
+
+export default function ErrorElement() {
+	let error = useRouteError();
+	let home = [
+		{
+			link_text: "Return home",
+			link_icon: "back",
+			link_url: "/",
+			link_target: "_self",
+		},
+	];
+	if (isRouteErrorResponse(error)) {
+		return (
+			<>
+				<div className="loading flex-column">
+					<h1>
+						{error.status} {error.statusText}
+					</h1>
+					<p>{error.data}</p>
+					<ProjectLinks links={home} />
+				</div>
+			</>
+		);
+	} else if (error instanceof Error) {
+		return (
+			<div>
+				<h1>Error</h1>
+				<p>{error.message}</p>
+				<p>The stack trace is:</p>
+				<pre>{error.stack}</pre>
+				<ProjectLinks links={home} />
+			</div>
+		);
+	} else {
+		return (
+			<div className="loading flex-column">
+				<h1>Unknown error</h1>
+				<ProjectLinks links={home} />
+			</div>
+		);
+	}
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,40 +1,42 @@
-import { StrictMode } from "react"
-import { createRoot } from "react-dom/client"
-import { RouterProvider } from "react-router-dom"
-import { createBrowserRouter } from "react-router"
-import DataAndMediaLoader from "./components/data/DataAndMediaLoader.js"
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import { createBrowserRouter } from "react-router";
+import DataAndMediaLoader from "./components/data/DataAndMediaLoader.js";
 
-import App from "./App"
-import Home from "./components/1-pages/Home"
-import Projects from "./components/1-pages/Projects"
-import LoadingElement from "./components/ux/LoadingElement"
+import App from "./App";
+import Home from "./components/1-pages/Home";
+import Projects from "./components/1-pages/Projects";
+import LoadingElement from "./components/ux/LoadingElement";
+import ErrorElement from "./components/ux/ErrorElement";
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <App/>,
-        loader: DataAndMediaLoader,
-        id: "root-data",
-        HydrateFallback: LoadingElement,
-        children: [
-            {
-                index: true,
-                Component: Home,
-            },
-            {
-                path: "projects",
-                Component: Projects,
-            },
-            {
-                path: "projects/:slug",
-                Component: Home,
-            }
-        ],
-    },
+	{
+		path: "/",
+		element: <App />,
+		loader: DataAndMediaLoader,
+		id: "root-data",
+		HydrateFallback: LoadingElement,
+		ErrorBoundary: ErrorElement,
+		children: [
+			{
+				index: true,
+				Component: Home,
+			},
+			{
+				path: "projects",
+				Component: Projects,
+			},
+			{
+				path: "projects/:slug",
+				Component: Home,
+			},
+		],
+	},
 ]);
 
-createRoot(document.getElementById('root')).render(
-    <StrictMode>
-        <RouterProvider router={router} />
-    </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+	<StrictMode>
+		<RouterProvider router={router} />
+	</StrictMode>,
+);
