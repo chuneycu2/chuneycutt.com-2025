450eb47f230d23295e599f08f7f35d39bdde24a8
Cyrus Huneycutt <chuneycu@gmail.com>
2026-01-05 16:11:16 -0500

    updates linter and dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,6 @@
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
@@ -12,11 +11,6 @@ export default defineConfig([
     languageOptions: {
       globals: globals.browser
     },
-    "rules": {
-      "react/react-in-jsx-scope": "off",
-      "react/jsx-uses-react": "off"
-    }
   },
   tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
 ]);
