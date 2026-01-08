fe6a8cbefe463e01364345e9af45579a63f1cd4d
Cyrus Huneycutt <chuneycu@gmail.com>
2026-01-08 10:03:59 -0500

    adds fade to social links to match rest of sidebar

--- src/components/5-atoms/SocialLinks.tsx
+++ src/components/5-atoms/SocialLinks.tsx
@@ -8,7 +8,7 @@ export default function SocialLinks(props) {
     const { email, github, linkedin }: Socials[] = props.links;
 
     return (
-        <aside className="social-links">
+        <aside className="social-links fade fade-right">
             {linkedin && <a href={linkedin} target="_blank"><IconLinkedIn /></a>}
             {github && <a href={github} target="_blank"><IconGitHub /></a>}
             {email && <a href={email}><IconEmail /></a>}
